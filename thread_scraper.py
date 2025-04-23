efc205fc4f18b6b292035be72fb3f7b8d2d70c18
jishnukoliyadan <83589431+jishnukoliyadan@users.noreply.github.com>
2025-04-24 00:51:01 +0530

    Add ThreadPoolExecutor-based scraper

---
+++ thread_scraper.py
@@ -0,0 +1,18 @@
+import requests
+from time import perf_counter
+from config import TOTAL_REQUESTS_COUNT
+from concurrent.futures import ThreadPoolExecutor
+
+
+def get_data(item_no):
+    r = requests.get(f"http://127.0.0.1:8000/products/{item_no}")
+    print(r.json())
+
+
+start = perf_counter()
+
+with ThreadPoolExecutor() as executor:
+    executor.map(get_data, range(1, TOTAL_REQUESTS_COUNT + 1))
+
+stop = perf_counter()
+print(f"Time taken : {stop - start}")
