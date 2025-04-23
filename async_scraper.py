24889a37e997ed486da610337e4c122ae895de76
jishnukoliyadan <83589431+jishnukoliyadan@users.noreply.github.com>
2025-04-24 01:29:35 +0530

    Add async scraper and update dependencies

---
+++ async_scraper.py
@@ -0,0 +1,26 @@
+import asyncio
+import aiohttp
+
+from time import perf_counter
+from config import TOTAL_REQUESTS_COUNT
+
+
+async def fetch_data(session, item_no):
+    r = await session.get(f"http://127.0.0.1:8000/products/{item_no}")
+    print(await r.json())
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        products = [] # List of tasks
+        for item_no in range(1, TOTAL_REQUESTS_COUNT + 1):
+            products.append(fetch_data(session, item_no)) # Collecting tasks
+        await asyncio.gather(*products) # Running all tasks concurrently
+
+
+start = perf_counter()
+
+asyncio.run(main())
+
+stop = perf_counter()
+print(f"Time taken : {stop - start}")
